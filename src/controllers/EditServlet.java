49b92bbd91e27f733eb8601f60c0dd948cae5f8c
yasuhiro.matsumoto <yaaaaabiyou@gmail.com>
2020-08-04 13:30:12 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -39,12 +39,15 @@ public class EditServlet extends HttpServlet {
 
         em.close();
 
-        // メッセージ情報とセッションIDをリクエストスコープに登録
+        /// タスク情報とセッションIDをリクエストスコープに登録
         request.setAttribute("message", m);
         request.setAttribute("_token", request.getSession().getId());
 
-        // メッセージIDをセッションスコープに登録
-        request.getSession().setAttribute("message_id", m.getId());
+        // タスクデータが存在しているときのみ
+        // タスクIDをセッションスコープに登録
+        if(m != null) {
+            request.getSession().setAttribute("message_id", m.getId());
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
